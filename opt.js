3d8588f7646d7aeb18597bb90b478d5ad966058c
ameerhamzashah <160713923+ameerhamzashah1@users.noreply.github.com>
2024-03-23 10:48:15 +0500

    these are my files of tsc and js

---
+++ opt.js
@@ -0,0 +1,20 @@
+// i am doing practice on logical operator:
+/*
+  use for (||) or
+  use for (!)  not
+  use for (&&) and
+  */
+var num1 = 5;
+var num2 = 6;
+var num3 = 1;
+console.log(num2 == 6 && num1 == 5);
+console.log(num3 == 1 || num2 == 6);
+console.log(num2 > 5 || num1 < num2);
+console.log(num1 > 4 && num2 < 10);
+console.log(!(num1 > 9));
+// Now False : 
+console.log(num1 && num2 == num3);
+console.log(!(num3 < 10));
+console.log(num1 === num2 || num3 === num1);
+console.log(num1 != num1);
+console.log(num1 && num2 !== num3);
---
+++ opt.ts
@@ -0,0 +1,22 @@
+// i am doing practice on logical operator:
+/*
+  use for (||) or
+  use for (!)  not
+  use for (&&) and
+  */
+
+  let num1 = 5;
+  let num2 = 6;
+  let num3 = 1;
+  console.log(num2==6 && num1==5);
+  console.log(num3==1||num2==6);
+  console.log(num2>5||num1<num2);
+  console.log(num1>4&&num2<10);
+  console.log(!(num1>9));
+
+  // Now False : 
+  console.log(num1&&num2==num3);
+  console.log(!(num3<10));
+  console.log(num1===num2||num3===num1);
+  console.log(num1!= num1);
+  console.log(num1&&num2!==num3);
